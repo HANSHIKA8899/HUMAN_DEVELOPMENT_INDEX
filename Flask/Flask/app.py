045d0ad21e1275ab263a801e2b4eb35fb498dca6
HANSHIKA8899 <hanshikavedulla8899@gmail.com>
2026-06-30 10:05:11 +0530

    Added requirements.txt

--- Flask/Flask/app.py
+++ Flask/Flask/app.py
@@ -1,21 +1,15 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
-import os
-print("Current Folder: ", os.getcwd())
-print("Files:", os.listdir())
 app = Flask(__name__)
 model = pickle.load(open('HDI.pkl', 'rb'))
 @app.route('/')
 def home():
-    return render_template('home.html')
-@app.route('/prediction')
-def prediction():
     return render_template('index.html')
 @app.route('/predict', methods=['POST'])
 def predict():
     features = [float(x) for x in request.form.values()]
     prediction = model.predict([features])
-    return render_template('index.html', prediction_text=f'🌍 Predicted Human Development Index (HDI): {prediction[0][0]:.4f}')
+    return render_template('index.html', prediction_text=f'Predicted HDI: {prediction[0][0]:.4f}')
 if __name__ == "__main__":
     app.run(debug=True)
